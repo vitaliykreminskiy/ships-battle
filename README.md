bb1a48b10a1b47a642fd922bbfff6a4cd2f1b18a
Vitaliy Kreminskyi <vkrmk13@gmail.com>
2021-07-13 17:28:33 +0300

    Added SeaMap layout rendering

---
+++ README.md
@@ -0,0 +1,6 @@
+Sea battle game!!
+
+To do list for now:
+
+[] Draw alphabet above sea map and numbers on the left
+[] Make an ability to draw/re-draw the SeaMap regarding to container width
--- src/components/SeaMap.tsx
+++ src/components/SeaMap.tsx
@@ -15,20 +15,51 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 )
 
+const topLetters = ' ABCDEFGHKL'
+
 //TODO: Bring out this config to separate object
+const frameRate = 10
 const mapDimension = 10
-const cellHeight = 50
-const cellWidth = 50
-const mapHeight = cellHeight * mapDimension
-const mapWidth = cellWidth * mapDimension
+const cellHeight = 45
+const cellWidth = 45
+const mapHeight = cellHeight * (mapDimension + 1)
+const mapWidth = cellWidth * (mapDimension + 1)
 
 const setup = ((p5: p5types, canvasParentRef: Element) => {
-  p5.createCanvas(mapWidth, mapHeight).parent(canvasParentRef)
+  p5.frameRate(frameRate)
+  //We need to add this single pixel to make the all line have the same weight
+  p5.createCanvas(mapWidth + 1, mapHeight + 1)
+    .parent(canvasParentRef)
+
+  p5.background(255)
+
+  //Drawing lines, horizontal and then vertical
+  p5.stroke(43, 177, 255)
+    .strokeWeight(1)
+  for (let i = 1; i <= mapDimension + cellWidth; i++) {
+    p5.line(cellWidth, cellHeight * i, mapWidth, cellHeight * i)
+  }
+
+  for (let i = 1; i <= mapDimension + cellHeight; i++) {
+    p5.line(cellWidth * i, cellHeight, cellWidth * i, mapWidth)
+  } 
+
+  //Drawing letters and numbers
+  for (let i = 1; i <= mapDimension + cellWidth; i++) {
+    p5.textAlign(p5.CENTER)
+      .textSize(cellHeight / 2)
+      .fill(43, 177, 255)
+      .text(topLetters[i], cellWidth * i, cellHeight / 4, cellWidth, cellHeight)
+  }
+
+  for (let i = 1; i <= mapDimension + 1; i++) {
+    p5.text(i, 0, cellHeight * i + (cellHeight / 4), cellWidth , cellHeight * i)
+  }
 })
 
 //TODO: Split drawing function
 const draw = (p5: p5types) => {
-  p5.background(255);
+  //
 };
 
 const SeaMap = () => {
